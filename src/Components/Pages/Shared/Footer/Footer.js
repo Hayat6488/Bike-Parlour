761efed324798b9fc88d601fb8a2795a8c7e2670
Hayat6488 <abulhayat619@gmail.com>
2022-11-25 20:19:43 +0600

    Footer added

--- src/Components/Pages/Shared/Footer/Footer.js
+++ src/Components/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+            <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded mt-16">
+                <div className="grid grid-flow-col gap-4">
+                    <Link>About us</Link>
+                    <Link>Contact us</Link>
+                    <Link>Terms & Conditions</Link>
+                </div>
+                <div>
+                    <p>Copyright © 2022 - All right reserved by DHEKUR | Taste of Bangladesh</p>
+                </div>
+            </footer>
         </div>
     );
 };
