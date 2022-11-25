3e6b10d40e339ede46b56745cc5251b20e65b9c0
Hayat6488 <abulhayat619@gmail.com>
2022-11-25 23:36:41 +0600

    register page done

--- src/Components/Pages/Register/Register.js
+++ src/Components/Pages/Register/Register.js
@@ -1,16 +1,11 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc'
-import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthProvider';
 
-const Login = () => {
+const Register = () => {
 
     const { signInByGoogle, setUser } = useContext(AuthContext);
-
-    const location = useLocation();
-    const navigate = useNavigate();
-
-    const from = location.state?.from?.pathname || '/';
 
     const handleGoogleSignIn = () => {
         signInByGoogle()
@@ -23,37 +18,52 @@ const Login = () => {
     }
 
     return (
-        <div className='w-full mt-6'>
+        <div className='w-full'>
+            <div class="flex justify-center items-center">
+            </div>
             <div className='flex justify-center items-center'>
                 <div className='bg-white px-16 py-8 rounded-lg shadow-lg shadow-indigo-500/40'>
                     <form className='mb-4'>
-                        <h1 className='text-3xl font-semibold'>Log In</h1>
-                        <div className='grid grid-cols-1 gap-4'>
-                            <div className='flex flex-col justify-start'>
+                        <h1 className='text-3xl font-semibold'>Register</h1>
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                            <div>
+                                <label className="label">
+                                    <span className="label-text text-2xl">Name: </span>
+                                </label>
+                                <input type="text" placeholder="FULL NAME" className="input input-bordered input-accent w-56 md:w-72" name='name' />
+                            </div>
+                            <div>
                                 <label className="label">
                                     <span className="label-text text-2xl">Email: </span>
                                 </label>
                                 <input type="Email" placeholder="EMAIL Address" className="input input-bordered input-accent w-56 md:w-72" name='email' />
                             </div>
-                            <div className='flex flex-col justify-start'>
+                            <div>
                                 <label className="label">
                                     <span className="label-text text-2xl">Password: </span>
                                 </label>
                                 <input type="Password" placeholder="PASSWORD" className="input input-bordered input-accent w-56 md:w-72" name='password' />
                             </div>
+                            <div>
+                                <label className="label">
+                                    <span className="label-text text-2xl">Image URL: </span>
+                                </label>
+                                <input type="text" placeholder="IMAGE URL" className="input input-bordered input-accent w-56 md:w-72" name='img' />
+                            </div>
                         </div>
                         <button type="submit" className='btn btn-outline btn-success mt-4 px-8 py-2 text-xl'>SUBMIT</button>
-                        <h2 className='text-xl mt-2'>Don't have an Account! Please <Link to='/register'><span className='text-blue-600'>Register</span></Link></h2>
+                        <h2 className='text-xl mt-2'>Already have an Account! Please <Link to='/login'><span className='text-blue-600'>Log In</span></Link></h2>
                     </form>
                     <hr />
                     <h1 className='text-2xl font-semibold mt-4 mb-4'>Log In With</h1>
-                    <div className='grid grid-cols-1 gap-2'>
+                    <div className='flex justify-center'>
                         <button onClick={handleGoogleSignIn} className="btn btn-outline text-xl"><FcGoogle className='mr-1'></FcGoogle> Log In By Google</button>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
 
-export default Login;
+export default Register;
