84b401285cfb2b6ec21a5e5dc79771ddbb368360
Hayat6488 <abulhayat619@gmail.com>
2022-12-01 20:16:25 +0600

    verify seller

--- src/Components/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Components/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
+import Buyer from './Buyer';
 
 const AllSellers = () => {
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+
+    const [buyers, setBuyers] = useState([]);
+
+    useEffect(() => {
+       fetch('http://localhost:5000/users/buyer')
+       .then(res => res.json())
+       .then(data => setBuyers(data))
+    },[ignored]) 
+
     return (
-        <div>
-            <h1>This is all sellers</h1>
+        <div className='flex justify-center w-full'>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th><span className='text-xl'>Name</span></th>
+                            <th><span className='text-xl'>Price</span></th>
+                            <th><span className='text-xl'>Status</span></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            buyers[0]?.map(buyer => <Buyer key={buyer._id} buyer={buyer} forceUpdate={forceUpdate}></Buyer>)
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                        <th><span className='text-xl'>Name</span></th>
+                            <th><span className='text-xl'>Price</span></th>
+                            <th><span className='text-xl'>Status</span></th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            </div>
         </div>
     );
-};
+};;
 
 export default AllSellers;
--- src/Components/Pages/Dashboard/AllSellers/Buyer.js
+++ src/Components/Pages/Dashboard/AllSellers/Buyer.js
@@ -19,6 +19,29 @@ const Buyer = ({ buyer, forceUpdate }) => {
 
     }
 
+    const handleVerify = id => {
+
+        const update = {
+            verified: true
+        }
+
+        fetch(`http://localhost:5000/users/buyer/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(update)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    console.log(data);
+                    forceUpdate();                   
+                }
+            })
+
+    }
+
     return (
         <tr>
             <td>
@@ -32,8 +55,11 @@ const Buyer = ({ buyer, forceUpdate }) => {
                 </div>
             </td>
             {
-                buyer?.verified ? <td><h1 className='text-green-500 text-xl'>Verified</h1></td> : <td><h1 className='text-red-700 text-xl'>Not Verified</h1></td> 
+                buyer?.verified ? <td><h1 className='text-green-500 text-xl'>Verified</h1></td> : <td><h1 className='text-red-700 text-xl'>Not Verified</h1></td>
             }
+            <td>
+                <button onClick={() => handleVerify(_id)} className='btn btn-ghost'>VERIFY</button>
+            </td>
             <td>
                 <button onClick={() => handleDelete(_id)} className='btn btn-ghost'>DELETE</button>
             </td>
