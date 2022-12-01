ac7b41866c1dafa1831f0bb9bd9a28bbdd64241c
Hayat6488 <abulhayat619@gmail.com>
2022-12-02 01:01:05 +0600

    reported items added

---
+++ src/Components/Pages/Dashboard/ReportedItems/Product.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Product = ({ bike, forceUpdate }) => {
+
+    console.log(bike);
+
+    const { _id, name, img, price } = bike;
+
+
+    const handleDelete = (_id) => {
+        const proceed = window.confirm('Are you sure you want to delete this Product?');
+        if (proceed) {
+            fetch(`http://localhost:5000/myproducts/${_id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    forceUpdate();
+                })
+        }
+    }
+
+        return (
+            <tr>
+                <td>
+                    <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                                <img src={img} alt="Avatar Tailwind CSS Component" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-bold">{name}</div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div>
+                        <div className="font-bold">{price}</div>
+                    </div>
+                </td>
+                <th>
+                    <button onClick={() => handleDelete(_id)} className="btn btn-ghost btn-xs"><span className="font-bold">DELETE</span></button>
+                </th>
+            </tr>
+        ); 
+    }
+
+export default Product;
--- src/Components/Pages/Dashboard/ReportedItems/ReportedItems.js
+++ src/Components/Pages/Dashboard/ReportedItems/ReportedItems.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
+import Product from './Product';
 
 const ReportedItems = () => {
+
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/products/reported')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    },[ignored])
+
     return (
-        <div>
-            <h1>This is Reported Items</h1>
+        <div className='flex justify-center w-full'>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th><span className='ml-16 text-xl'>Name</span></th>
+                            <th><span className='text-xl'>Price</span></th>
+                            <th><span className='text-xl'>Action</span></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products?.map(bike => <Product key={bike?._id} bike={bike} forceUpdate={forceUpdate}></Product>)
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                        <th><span className='ml-16 text-xl'>Name</span></th>
+                            <th><span className='text-xl'>Price</span></th>
+                            <th><span className='text-xl'>Action</span></th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            </div>
         </div>
     );
 };
